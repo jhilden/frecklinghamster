7e4600a77e105cf1040f0e66ad0d2e6eed017ad0
Jakob Hilden <jakobhilden@gmail.com>
2012-11-02 19:09:30 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frecklinghamster::Application.config.secret_token = '5b40b9e5d3a710918b15d11bc7edb630df22b74a4293212235e7534b903b736f31a9208682b5d09ff9745ea40e7ac0ca1b6f0c36bb2370471a35831a5b609467'
